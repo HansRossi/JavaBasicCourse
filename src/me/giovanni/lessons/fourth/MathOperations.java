5a5a1f973bea35b80f375584e38810986a003201
Giovanni <ivanalipov4@gmail.com>
2025-04-27 22:29:10 +0200

    Add math operations

---
+++ src/me/giovanni/lessons/fourth/MathOperations.java
@@ -0,0 +1,14 @@
+package me.giovanni.lessons.fourth;
+
+public class MathOperations {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 6;
+        double result = a + b;
+        System.out.println(result);
+        result = a / (double) b;
+        System.out.println(result);
+
+        int reminder = a /  b;
+    }
+}
